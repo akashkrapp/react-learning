fdd53054b9f6b928c5ef85bb3f031265e21c4373
@nitin.srivastava <nitin.srivastava@appinventiv.com>
2024-07-01 19:28:30 +0530

    use state component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../common/header';
 import Sidebar from '../../common/sidebar';
+import { Box } from '@mui/material';
 
 
 
 function Dashboard() {
+  const [
+    name, setName
+  ] = useState()
+  
   return (
     <div className="pageMainWrapper">
         <Header/>
-        <Sidebar/>
+        
         <div className="innerWrapper">
-            <p>content here</p>
+        <Sidebar/>
+            <Box>
+                <div className="userBox">
+                  <p className="data">
+                    type here
+                  </p>
+                  <input type="text" value={name} onChange={(e:any)=> setName(e.target.value)} />
+                 <p>{name}</p>
+                </div>
+            </Box>
         </div>
       
     </div>
